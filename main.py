759250641302f28360ca7d02e7a151886e731bf7
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-11-24 22:02:28 +0545

    encrypt

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ class Caeser:
     def __init__(self, shift) -> None:
         self.shift = shift
         
-    def encrypt() ->str:
-        pass
+    def encrypt(self,message) ->str:
+        shift = self.shift % 26
+        result = ''
+        for char in message:
+            if 'A' <= char <= 'Z':
+                result += char((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            elif 'a' <= char <='z':
+                result += char((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                result += char
+        return result
     
-    def decrypt() -> str:
+    def decrypt(self,message) -> str:
         pass
 
 def main():
@@ -13,7 +22,8 @@ def main():
         caeser = Caeser(9) # you can use shift value from user also
         choice = int(input("Choose mode: 1. Encrypt 2. Decrypt"))
         if choice == 1:
-            pass
+            message = input("Enter the message to encrypt")
+            result = caeser.encrypt(message)
         elif choice == 2:
             pass
         else:
